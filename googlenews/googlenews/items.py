537ba5d58689dac6ccaaa6cc30a9b22546e89957
qnods <edward@qnods.com>
2021-08-17 01:08:11 +0800

    upload v0.4a - check readme for detail

--- googlenews/googlenews/items.py
+++ googlenews/googlenews/items.py
@@ -7,14 +7,59 @@ import scrapy
 from scrapy.loader import ItemLoader
 from itemloaders.processors import TakeFirst, MapCompose
 from w3lib.html import remove_tags
+import datetime
 
-def split_date(value):
-    newdate = value.split()
-    return newdate[1] + ' ' + newdate[2] + ' ' + newdate[3]
+# activate this part ONLY when scrap from rss
+# def split_date(value):
+#     newdate = value.split()
+#     return newdate[1] + ' ' + newdate[2] + ' ' + newdate[3]
+
+month={
+    "1":"Jan",
+    "2":"Feb",
+    "3":"Mar",
+    "4":"Apr",
+    "5":"May",
+    "6":"Jun",
+    "7":"Jul",
+    "8":"Aug",
+    "9":"Sep",
+    "10":"Oct",
+    "11":"Nov",
+    "12":"Dec"
+}
 
 def strip_item(value):
     excerpt = value.strip(",")
     return excerpt
+
+def convert_month(value):
+    old_date = value.split("/")
+    new_date = old_date[1] + "-" + month[old_date[0]] + "-" + old_date[2]
+
+    return new_date
+    
+def convert_pubdate(value):
+    modifier = 0
+    old_date = value.split()
+    day_differences = modifier*int(old_date[0])
+    today = datetime.date.today()
+    date_reducer = datetime.timedelta(days = day_differences)
+    if 'hour' in value or 'hours' in value:
+        new_date = (today - date_reducer).strftime('%d %b %Y')
+    elif 'day' in value or 'days' in value:
+        modifier=1
+        new_date = (today - date_reducer).strftime('%d %b %Y')
+    elif 'week' in value or 'weeks' in value:
+        modifier=7
+        new_date = (today - date_reducer).strftime('%d %b %Y')
+    elif 'month' in value or 'months' in value:
+        modifier=30
+        new_date = (today - date_reducer).strftime('%d %b %Y')
+    else:
+        new_date = value
+    
+    return new_date
 
 
 class GooglenewsItem(scrapy.Item):
@@ -22,8 +67,10 @@ class GooglenewsItem(scrapy.Item):
     query = scrapy.Field()
     region = scrapy.Field()
     title = scrapy.Field(input_processor = MapCompose(remove_tags), output_processor = TakeFirst())
-    date = scrapy.Field(input_processor = MapCompose(remove_tags, split_date), output_processor = TakeFirst())
+    date = scrapy.Field(input_processor = MapCompose(remove_tags, convert_pubdate), output_processor = TakeFirst())
     source = scrapy.Field(input_processor = MapCompose(remove_tags), output_processor = TakeFirst())
     link = scrapy.Field(input_processor = MapCompose(remove_tags), output_processor = TakeFirst())
-    excerpt = scrapy.Field(input_processor = MapCompose(strip_item), output_processor = TakeFirst())
+    excerpt = scrapy.Field(input_processor = MapCompose(strip_item, remove_tags), output_processor = TakeFirst())
+    start = scrapy.Field(input_processor = MapCompose(convert_month), output_processor = TakeFirst())
+    end = scrapy.Field(input_processor = MapCompose(convert_month), output_processor = TakeFirst())
     
---
+++ googlenews/googlenews/spiders/gspider3.py
@@ -0,0 +1,33 @@
+import scrapy
+from googlenews.items import GooglenewsItem
+from scrapy.loader import ItemLoader
+
+class gSpider(scrapy.Spider):
+    name= 'gnews3'
+    # start_urls = ['https://www.google.com/search?q=semiconductor&biw=1920&bih=969&tbm=nws&ei=yJUaYbiTEdLarQHR6p-ADw&oq=semiconductor&gs_l=psy-ab.3..0i67k1l2j0i433i131i67k1j0i512i433i395k1j0i433i67i395k1j0i67i395k1l5.4614.5162.0.6646.2.2.0.0.0.0.174.315.0j2.2.0..3..0...1.1.64.psy-ab..0.2.315...0i512i395k1.0.Z2oLMrWsiR0']
+    def start_requests(self):
+        yield scrapy.Request(f'https://www.google.com/search?q={self.query}&rlz=1C1ONGR_en{self.region}933SG933&tbs=cdr:1,cd_min:{self.start},cd_max:{self.end},sbd:1&tbm=nws&sxsrf=ALeKk01aflpQNP1ZZ_T4be6c1j0AaGca-g:1629088758656&source=lnt&sa=X&ved=2ahUKEwiVoJHG3LTyAhUIA3IKHUJTA3gQpwV6BAgHECw&biw=1920&bih=969&dpr=1')
+    
+    
+    def parse(self, response):
+        for articles in response.css('div.dbsr'):
+            l = ItemLoader(item = GooglenewsItem(), selector=articles)
+
+            l.add_css('title', 'div.JheGif.nDgy9d')
+            l.add_css('excerpt', 'div.Y3v8qd')
+            l.add_css('source','div.XTjFC.WF4CUc')
+            l.add_css('date', 'span.WG9SHc span')
+            l.add_value('query', self.query)
+            l.add_value('start', self.start)
+            l.add_value('end',self.end)
+            l.add_value('region', self.region)
+            l.add_css('link','a::attr(href)')
+
+            yield l.load_item()
+
+
+        # nextPage = response.css('[id="pnnext"]').attrib['href']
+        # nextLink = "https://google.com" + nextPage
+
+        # if nextPage is not None:
+        #     yield response.follow(nextLink, callback=self.parse)
